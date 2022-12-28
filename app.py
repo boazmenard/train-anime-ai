81c7da08a552a3be1c1e4bbb3bcf727b0233f727
Boaz Menard <42014936+boazmenard@users.noreply.github.com>
2022-12-28 01:54:36 +0000

    add columns and gif

--- app.py
+++ app.py
@@ -2,23 +2,26 @@
 import streamlit as st
 from helpers import generateWorkoutPlan
 
-col1, col2, col3 = st.columns(3)
+st.image("https://images.genius.com/4027d128a604c47448e3cca96814020d.300x220x66.gif")
+st.title('TrainAnimeAI')
+# col1, col2, col3 = st.columns(3)
+# with col1:
+#     st.write(' ')
+# with col2:
+#     st.image("https://images.genius.com/4027d128a604c47448e3cca96814020d.300x220x66.gif")
+#     st.title('TrainAnimeAI')
+# with col3:
+#     st.write(' ')
 
-with col1:
-    st.write(' ')
-
-with col2:
-    st.title('TrainAnimeAI')
-    # st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.write(' ')
-
-#st.title('TrainAnimeAI')
-
+st.write('Train like your favorite anime character. Use the power of anime to achieve fitness immortality.')
 anime_character = st.text_input("Enter Your Anime Muse:", placeholder="Sasuke Uchiha")
 anime_title = st.text_input("Enter The Anime Title:", placeholder="Naruto")
 
 if st.button("Get Workout Plan"):
-    workout_plan = generateWorkoutPlan(anime_character, anime_title)
-    st.write(workout_plan)
+    col4, col5 = st.columns(2)
+    with col4:
+        workout_plan = generateWorkoutPlan(anime_character, anime_title)
+        st.header('Training Plan')
+        st.write(workout_plan)
+    with col5:
+        st.header('Cosplay This Character')
