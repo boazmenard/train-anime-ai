5434e91c562a9199ee391e501b928b4d0c5814c6
Boaz Menard <42014936+boazmenard@users.noreply.github.com>
2022-12-30 00:00:37 +0000

    switch prompt

--- helpers.py
+++ helpers.py
@@ -13,7 +13,6 @@ def generateWorkoutPlan(anime_character, anime_title):
     prompt = f'''
     Give me a weekly workout and diet plan that will give me the body of the anime character below. I want the exercises, sets, and reps and breakfast, lunch, dinner, and snacks. I do not want to workout longer than 1 hour each day that I workout.
     Anime Character: {anime_character} from the anime {anime_title}/n
-    Print the workout and the diet plan out line by line./n
     '''
     response = openai.Completion.create(
         engine="text-davinci-003",
